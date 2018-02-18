c75e5b21bebb6d5dc1024947c4abd3acc7963bcd
Yota Nagahata <nagahata-yota@c-fo.com>
2018-02-18 20:54:12 +0900

    Make recursive comment type to pointer

--- decode/comment.go
+++ decode/comment.go
@@ -14,7 +14,7 @@ type Comment struct {
 	From         UserID       `json: from`
 	Message      string       `json: message`
 	MessageTags  []MessageTag `json: message_tags`
-	Parent       Comment      `json: parent`
+	Parent       *Comment     `json: parent`
 	PostID       PostID       `json: post_id`
 	ParmalinkUrl string       `json: parmalink_url`
 	Verb         Verb         `json: verb`
